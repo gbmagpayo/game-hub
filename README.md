01b9b48dc4b5941cb64601dd2e324d8bdf45d34e
OyoMagpayo <80026713+OyoMagpayo@users.noreply.github.com>
2023-10-06 15:57:46 +0800

    Hangman code

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Game Hub
+
+## Hangman Program flow
+1. start
+2. generate a random word
+3. display: secretword, guessed letters, number of incorrect guessed left
+4. ask user for an input
+5. check if input is a letter guess or a word guess
+    - if a letter guess
+        1. check if it is already guessed (prompt again if true)
+        2. check if letter in secret word
+            - if true: add letter in guessed letters
+            - if false: add letter in guess letters, subtract 1 from number of incorrect guesses
+    - if a word guess:
+        1. check if word guess is equal to secretword
+            - if true: end and display correct guess
+            - if false: subtract 1 from number of incorrect guesses
+
+note: the game ends if player consumed the number of incorrect guesses or if the player was able to guess the word
+
+
---
+++ hangman.py
@@ -0,0 +1,83 @@
+import random
+
+def generateRandomWord():
+    ''' Generates a random word from a txt.file'''
+    with open("words.txt", "r") as file:
+        words = [word.strip() for word in file]
+    return random.choice(words)
+
+def getGuess(secretWord, guessedLetters):
+    while True: 
+        guess = input("Enter guess: ").lower()
+
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessedLetters:
+                print("Letter is already guessed")
+            else:
+                return guess, False
+    
+        elif len(guess) == len(secretWord):
+            return guess, True
+        
+        else:
+            print("Invalid input")
+
+def displayGame(secretWord, guessedLetters, numTriesLeft):
+    print("Letters guessed:", ", ".join(guessedLetters))
+    print(f"Tries left: {numTriesLeft}")
+    for letter in secretWord:
+        if letter in guessedLetters:
+            print(letter, end = " ")
+        else:
+            print("_", end = " ")
+    print()
+
+
+def hangman():
+    secretWord = generateRandomWord()
+
+    guessedLetters= []
+    numTriesLeft = len(secretWord) - 2
+
+    #inside while true
+    while True:
+        if numTriesLeft == 0:
+            print("Nice try")
+            break
+
+        if all(letter in guessedLetters for letter in secretWord):
+            print("You guessed the word!")
+            break
+
+        displayGame(secretWord, guessedLetters, numTriesLeft)
+        guess, isGuessWord = getGuess(secretWord, guessedLetters)
+
+        if isGuessWord == True:
+            if guess == secretWord:
+                print("You guessed the word!")
+                break
+            else:
+                print("Wrong Guess")
+                numTriesLeft -= 1
+        else:
+            if guess in secretWord:
+                guessedLetters.append(guess)
+            else:
+                guessedLetters.append(guess)
+                numTriesLeft -= 1
+    
+    
+
+
+
+def main():
+    hangman()
+
+    
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
